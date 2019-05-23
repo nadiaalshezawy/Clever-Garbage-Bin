b7d1a0b063ad99c48e1a8df7b270ad78c597b3c2
Nadia Ahmed <nadiaalshezawy@gmail.com>
2019-05-23 16:14:17 +0400

    save readin to the table

--- scanapplication.py
+++ scanapplication.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import exists
 from database_setup import MeasurementWaste, Base,User,MeasurementRecycle 
 from flask import session as login_session
+from datetime import datetime
 
 app = Flask(__name__)
 APPLICATION_NAME = "scan application"
@@ -25,8 +26,29 @@ def showMain():
     #return "This page will show main page catalog"
 
 # new reading
-@app.route('/addreading')
+@app.route('/addreading', methods=['GET', 'POST'])
 def addReading():
+	if request.method == 'POST':
+		barcode = request.form['barcode']
+		weight =request.form['weight']
+		print(barcode + " "+barcode[0]+" "+weight )
+		#save the reading in recycle table in barcode start with R
+		if barcode[0]=="R":
+			readings= session.query(MeasurementRecycle).all()
+			newreading = MeasurementRecycle(id=3, date=datetime.now(),barcode=request.form['barcode'], weight=request.form['weight']);
+			session.add(newreading)
+			session.commit()
+			flash("new reading added!")
+			print("recycle")
+		#save the reading in waste table in barcode start with W
+		elif barcode[0]=="W":
+			readings= session.query(MeasurementWaste).all()
+			newreading = MeasurementWaste(id=4, date=datetime.now(),barcode=request.form['barcode'], weight=request.form['weight']);
+			session.add(newreading)
+			session.commit()
+			flash("new reading added!")
+			print("Waste")
+		return redirect(url_for('addReading'))
 	return render_template('addReading.html')
     #return "This page will add reading"
 
